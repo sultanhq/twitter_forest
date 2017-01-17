4d473d39d8d2359e5f79d47ef3d18d1123f2e5e9
ajbater <amanda22@hotmail.co.uk>
2017-01-17 10:26:27 +0000

    twitter gem setup in progress

---
+++ config/initializers/twitter_credentials.rb
@@ -0,0 +1,8 @@
+require 'twitter'
+
+Twitter.configure do |config|
+  config.consumer_key = ENV['CONSUMER_KEY']
+  config.consumer_secret = ENV['CONSUMER_SECRET']
+  config.oauth_token = ENV['TWITTER_ACCESS_TOKEN']
+  config.oauth_token_secret = ENV['TWITTER_ACCESS_SECRET']
+end
