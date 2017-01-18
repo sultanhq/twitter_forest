767cad69f7290c4cb2cf65a27e6def3c763c7d93
Dan Pelensky <dan@pelensky.com>
2017-01-18 16:15:48 +0000

    adds selection to the nav bar

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
  $("#moving_bar").css("height",(`${parseInt(document.getElementById("counter").innerHTML)}%`))
 };
+
+$(document).ready(function(){
+    $("#generate-forest").click(function(){
+      var selection = $('#forest-selection').find(":selected").text();
+      console.log()
+        $("#forest-type").text(`of ${selection}`);
+    });
+});
