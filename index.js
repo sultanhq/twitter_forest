87f5174abe0814f9c6679343aa29cd6cffe1fd0c
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-18 15:57:58 +0000

    added background audio fixes #32 fixes #34

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
  $("#moving_bar").css("height",(`${parseInt(document.getElementById("counter").innerHTML)}%`))
 };
+
+bgAudio = new Audio('./public/media/bgAudio.mp3');
+bgAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+bgAudio.volume = 0.25;
+bgAudio.play();
