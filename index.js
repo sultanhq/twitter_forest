e939135c73b17b4770e41e2bb259673d4952f6ab
ajbater <amanda22@hotmail.co.uk>
2017-01-20 13:57:44 +0000

    adds multiple trees

--- index.js
+++ index.js
@@ -9,18 +9,46 @@ window.onload = function() {
     var counter = document.getElementById("counter");
     if (counter.innerHTML < 15) {
       document.getElementById("tree").className = "tree-1";
+      document.getElementById("tree-2").className = "tree-1";
+      document.getElementById("tree-3").className = "tree-1";
+      document.getElementById("tree-4").className = "tree-1";
+      document.getElementById("tree-5").className = "tree-1";
     } else if (counter.innerHTML >= 15 && counter.innerHTML < 30) {
       document.getElementById("tree").className = "tree-2";
+      document.getElementById("tree-2").className = "tree-2";
+      document.getElementById("tree-3").className = "tree-2";
+      document.getElementById("tree-4").className = "tree-2";
+      document.getElementById("tree-5").className = "tree-2";
     } else if (counter.innerHTML >= 30 && counter.innerHTML < 45) {
       document.getElementById("tree").className = "tree-3";
+      document.getElementById("tree-2").className = "tree-3";
+      document.getElementById("tree-3").className = "tree-3";
+      document.getElementById("tree-4").className = "tree-3";
+      document.getElementById("tree-5").className = "tree-3";
     } else if (counter.innerHTML >= 45 && counter.innerHTML < 60) {
       document.getElementById("tree").className = "tree-4";
+      document.getElementById("tree-2").className = "tree-4";
+      document.getElementById("tree-3").className = "tree-4";
+      document.getElementById("tree-4").className = "tree-4";
+      document.getElementById("tree-5").className = "tree-4";
     } else if (counter.innerHTML >= 60 && counter.innerHTML < 75) {
       document.getElementById("tree").className = "tree-5";
+      document.getElementById("tree-2").className = "tree-5";
+      document.getElementById("tree-3").className = "tree-5";
+      document.getElementById("tree-4").className = "tree-5";
+      document.getElementById("tree-5").className = "tree-5";
     } else if (counter.innerHTML >= 75 && counter.innerHTML < 90) {
       document.getElementById("tree").className = "tree-6";
+      document.getElementById("tree-2").className = "tree-6";
+      document.getElementById("tree-3").className = "tree-6";
+      document.getElementById("tree-4").className = "tree-6";
+      document.getElementById("tree-5").className = "tree-6";
     } else {
       document.getElementById("tree").className = "tree-7";
+      document.getElementById("tree-2").className = "tree-7";
+      document.getElementById("tree-3").className = "tree-7";
+      document.getElementById("tree-4").className = "tree-7";
+      document.getElementById("tree-5").className = "tree-7";
     }
   }, 3000);
 
